512754ad1f1f512e5f8acf956a57826a57f64532
MDSAJIBCHOWDHURY <mdsajibcse14@gmail.com>
2021-10-04 07:01:43 -0700

    asset image added

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: Scaffold(
       appBar: AppBar(
         title: Text('I am Rich'),
       ),
       body: Center(
-        child: Image.network(
-            'https://pbs.twimg.com/profile_images/1141781661551665153/BMnvVd2u_400x400.jpg'),
+        child: Image.asset('images/login_image.png'),
       ),
     ),
   ));
